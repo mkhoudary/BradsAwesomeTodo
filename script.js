d3e7ca18045ff2a0318fb1332115d08abe82962c
Shaban Olwan <sh.3ban@live.com>
2019-08-22 15:27:50 +0300

    add day and time of task

--- script.js
+++ script.js
@@ -102,24 +102,68 @@ $(document).ready(function () {
         editable.after(editableInput);
     });
 
+    function getre(str, num) {
+        if (str === num) return 'nice try';
+        var res = [/^\D+/g, /\D+$/g, /^\D+|\D+$/g, /\D+/g, /\D.*/g, /.*\D/g, /^\D+|\D.*$/g, /.*\D(?=\d)|\D+$/g];
+        for (var i = 0; i < res.length; i++)
+            if (str.replace(res[i], '') === num)
+                return 'num = str.replace(/' + res[i].source + '/g, "")';
+        return 'no idea';
+    }
+
+
+    var getStartDate = function (taskText) {
+        const today = new Date();
+        var chunks = taskText.split(" ");
+        for (var i = 0; i < chunks.length; i++) {
+            var chunk = chunks[i];
+            //13:00 or 1:00 or 5:05 or 5:5
+            if (chunk.match("^([0-9]|0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]|1[0-5]|[0-9])$")) {
+                var chunkSplits = chunk.split(":");
+                today.setHours(parseInt(chunkSplits[0]));
+                today.setMinutes(parseInt(chunkSplits[1]));
+            }
+        }
+        var dateNum = getDay(taskText);
+        today.setDate(dateNum);
+        return today;
+    };
+    var getDay = function (taskText) {
+        var chunks = taskText.split(" ");
+        var weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        for (var i = 0; i < chunks.length; i++) {
+            var chunk = chunks[i];
+            for (var j = 0; j < weekDays.length; j++) {
+                var day = weekDays[j];
+                if (chunk === day || chunk === day.substring(0, 3) || chunk === day.substring(0, 4)) {
+                    var d = new Date();
+                    return d.getDate() + (7 + j - d.getDay()) % 7;
+                }
+            }
+        }
+    };
+    //Detect Task Text Changed
     var detectEditableInputChange = function (event) {
         var container = $("input.editable-input");
-        // if the target of the click isn't the container nor a descendant of the container
         if (!container.is(event.target) && container.has(event.target).length === 0 && container.is(":visible")) {
             var changedValue = container.val();
             var editable = container.parent().find('.editable');
-            editable.text(changedValue);
+            if (changedValue != null && changedValue != '') {
+                editable.text(changedValue);
+                var startDate = getStartDate(changedValue);
+                if (!isNaN(startDate.getDay())) {
+                    editable.parent().find('.start-date').remove();
+                    editable.after('<time class="timeago start-date" datetime="' + startDate.toISOString() + '"></time>');
+                }
+            }
             editable.parent().find(":not(input)").removeClass('hidden');
             container.remove();
             saveDetails();
         }
     };
-    $('body').mouseup(function (event) {
-        detectEditableInputChange(event);
-    });
-
-    $('body').keypress(function (event) {
-        if (event.which == 13) {
+    $('body').on('mouseup keypress', function (event) {
+        if (event.type == 'mouseup') detectEditableInputChange(event);
+        else if (event.type == 'keypress' && event.which == 13) {
             detectEditableInputChange(event);
         }
     });
@@ -154,7 +198,8 @@ function saveDetails() {
                 title: $(this).find("span").text(),
                 createdTime: $(this).find(".start-time").attr("datetime"),
                 endTime: $(this).find(".done-time").attr("datetime"),
-                startedTime: $(this).find(".started-time").attr("datetime")
+                startedTime: $(this).find(".started-time").attr("datetime"),
+                startDate: $(this).find(".start-date").attr("datetime"),
             };
         });
 
@@ -246,6 +291,10 @@ function reloadSelectedTodo(externalContent) {
                 var endTime = (item.endTime !== 'undefined' && item.endTime !== undefined ? ('<time class="timeago done-time" datetime="' + item.endTime + '"></time>') : '');
                 var startedTime = (item.startedTime !== 'undefined' && item.startedTime !== undefined ? ('<time class="timeago started-time" datetime="' + item.startedTime + '"></time>') : '');
                 var spanItem = '<span class="editable">' + item.title + '</span>';
+                //
+                // + '<time class="timeago start-time" datetime="' + item.startDate + '"></time>'
+                // + (startDate)
+                // + '</time>'
                 var listItem = '<li>' + spanItem + '<time class="timeago start-time" datetime="' + item.createdTime + '"></time>' + (endTime !== "" ? endTime : startedTime) + ($("." + listId).hasClass("details-list") || $("." + listId).hasClass("archive-list") ? '' : '<i class="fa fa-archive" onclick="archiveParent(event);"></i>') + '<i class="fa fa-trash-o" onclick="removeParent(event);"></i></li>';
                 $("." + listId).append(listItem);
             });
